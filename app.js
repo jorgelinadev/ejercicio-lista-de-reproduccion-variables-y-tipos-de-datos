584d9f996b271180ec223972bcab16e733b653a5
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 10:00:41 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//alert ("Mi archivo está bien linkeado");
+
+const nombrePlayList = prompt ('Ingresa un nombre para tu nueva Play List');
+const cancion1 = prompt ('Ingresa tu primera canción');
+const cancion2 = prompt ('Ingresa una nueva canción');
+const cancion3 = prompt ('Ingresa una nueva canción');
+
+alert ('Se ha creado una nueva Play List llamada: ' + nombrePlayList +' con las canciones: '+ cancion1 + ', ' + cancion2 +' y '+ cancion3+'.');
+
+console.log (alert);
